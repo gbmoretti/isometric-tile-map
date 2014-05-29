7f100795693c7e425aa11ad12ae50fabf5a78457
Guilherme Moretti <guilherme.moretti@innvent.com.br>
2014-05-29 01:48:24 -0300

    new position class

---
+++ js/position.js
@@ -0,0 +1,17 @@
+function Position(x,y,z) {
+  this.x = parseInt(x);
+  this.y = parseInt(y);
+  this.z = parseInt(z);
+};
+
+Position.prototype.equals = function(other) {
+  if(other === undefined) return this;
+  return (this.x === other.x) &&
+        (this.y === other.y) &&
+        (this.z === other.z)
+};
+
+Position.prototype.equals2d = function(other) {
+  if(other === undefined) return this;
+  return (this.x === other.x) && (this.y === other.y);
+}
